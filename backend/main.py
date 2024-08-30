e9982f3691ae4b8678279fef3cd17919bf664bcc
Joshua Zhou <j.zhou11@share.epsb.ca>
2024-08-30 17:59:35 -0400

    ready for server link

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,12 @@
 from fastapi import FastAPI
 from enum import Enum
-
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from config import password
 
 app = FastAPI()
 
-uri = "mongodb+srv://admin:<db_password>@cluster0.6jhzc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
 
 
-
-
-
-
-
-
+"""
 class ModelName(str, Enum):
     alexnet = "alexnet" # name: value
     resnet = "resnet"
@@ -75,3 +59,5 @@ async def option(q: str | None = None):
     if q:
         return {"q" : q}
     return {"no q"}
+
+"""
