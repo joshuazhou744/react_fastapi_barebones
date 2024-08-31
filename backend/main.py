0d52c9cc0d49ea5db1d287a65e55aa550e52ee0e
Joshua Zhou <j.zhou11@share.epsb.ca>
2024-08-31 16:02:29 -0400

    connect api to backend and create get, post, update, delete methods

--- backend/main.py
+++ backend/main.py
@@ -1,69 +1,76 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from enum import Enum
-from pymongo import MongoClient
-from config import password
-from model import Climb
+from fastapi import FastAPI
+import motor.motor_asyncio
+import pprint
 
-from database import (
-    fetch_one_climb,
-    fetch_all_climbs,
-    create_climb,
-    update_climb,
-    remove_climb,
-)
+from config import password # gitignored, contains password
+from model import Climb
+from enum import Enum
 
 connection_string = f"mongodb+srv://admin:{password}@cluster0.6jhzc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(connection_string)
+client = motor.motor_asyncio.AsyncIOMotorClient(connection_string)
 
 app = FastAPI()
 
-origins = ['https://localhost:3000']
+def get_db():
+    return client["test_db"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"],
-)
+def get_climb_collection():
+    db = get_db()
+    collection = db["climbs"]
+    return collection
 
-@app.get("/")
-async def read_root():
-    return {"ping":"pong"}
 
-@app.get("/api/climb")
-async def get_climb():
-    response = await fetch_all_climbs()
-    return response
-
-@app.get("/api/climb{title}", response_model=Climb)
-async def get_climb_by_title(title):
-    response = await fetch_one_climb(title)
-    if response:
-        return response
-    raise HTTPException(404, f"there is no climb item with the title: {title}")
-
-@app.post("/api/climb", response_model=Climb)
+@app.post("/climbs/")
 async def post_climb(climb: Climb):
-    response = await create_climb(climb.dict())
-    if response:
-        return response
-    raise HTTPException(400, "something went wrong, bad request")
+    document = climb.dict()
+    collection = get_climb_collection()
+    result = await collection.insert_one(document)
+    return str(result.inserted_id)
 
-@app.put("/api/climb{title}", response_model=Climb)
-async def put_climb(title:str, content:str):
-    response = update_climb(title, content)
-    if response:
-        return response
-    raise HTTPException(404, f"there is no climb item with the title: {title}")
+@app.put("/climbs/{title}/{climb_id}")
+async def add_id(title: str, climb_id: int):
+    collection = get_climb_collection()
+    old_document = await collection.find_one({"title": title})
+    print("found document: %s" % pprint.pformat(old_document))
+    _id = old_document["_id"]
+    result = await collection.replace_one({"_id": _id}, {"climb_id": climb_id, **old_document})
+    print("replaced %s document" % result.modified_count)
+    new_document = await collection.find_one({"_id": _id})
+    print("document is now %s" % pprint.pformat(new_document))
+    return str(new_document)
 
-@app.delete("/api/climb{title}")
-async def del_climb(title):
-    response = await remove_climb(title)
-    if response:
-        return f"Successfully removed item"
-    raise HTTPException(404, f"there is no climb item with the title: {title}")
+@app.get("/climbs/{title}")
+async def test_get(title: str):
+    collection = get_climb_collection()
+    result = await collection.find_one({"title": title})
+    if result is None:
+        result = ("404 not found")
+    pprint.pprint(result)
+    return str(result)
+
+
+@app.put("/climbs/{climb_id}/{new_title}/")
+async def change_title(climb_id: int, new_title: str):
+    collection = get_climb_collection()
+    result = await collection.update_one({"climb_id": climb_id}, {"$set": {"title": new_title}})
+    print("updated %s document" % result.modified_count)
+    new_document = await collection.find_one({"title": new_title})
+    print("document is now %s" % pprint.pformat(new_document))
+    return str(new_document)
+
+@app.delete("/climbs/")
+async def del_climb(title: str | None = None, climb_id: int | None = None):
+    collection = get_climb_collection()
+    n = await collection.count_documents({})
+    print("%s documents before calling delete_one()" % n)
+    if title is not None:
+        result = await collection.delete_one({"title": title})
+    else:
+        result = await collection.delete_one({"climb_id": climb_id})
+    print("%s documents after" % (n))
+    return str(result)
+
+
 
 
 """
--- backend/model.py
+++ backend/model.py
@@ -3,3 +3,4 @@ from pydantic import BaseModel
 class Climb(BaseModel):
     title: str
     content: str
+    grade: str | None = None
